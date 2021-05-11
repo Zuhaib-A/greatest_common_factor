18260e02e063ee9a09d796c1accbc523c6e9a472
Zuhaib-A <83922659+Zuhaib-A@users.noreply.github.com>
2021-05-11 15:58:26 +0100

    Update and rename modulus.py to gcf.py

--- gcf.py
+++ gcf.py
@@ -1,7 +1,7 @@
+#Using the modulus operator in python to find the greatest common factor between two numbers
 number_1=int(input("Input a number:\n"))
 number_2=int(input("Input another number:\n"))
 numbers=(number_1, number_2)
-
 def gcf():
     numbs=sorted(numbers, reverse=True)
     big=numbs[0]
@@ -14,9 +14,7 @@ def gcf():
         print(mod)
         big=small
         small=mod
-        
     print("The greatest common factor between " + str(numbs[0]) + " and " + str(numbs[1]) + " is " + str(big) + ".")
-   
 gcf()
 
 
